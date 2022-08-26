a26f16f1ae7733c93b73e6df79ae20d61cd62f9a
Cyril Gadal <cyril.gadal@gmail.com>
2022-08-26 12:36:24 +0200

    gallery template that works

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -3,7 +3,13 @@ const data = [
     src: '/videos/impact_crater.mp4',
   },
   {
-    src: '/images/manip_video.mp4',
+    src: '/videos/manip_video.mp4',
+  },
+  {
+    src: '/images/projects/DunePanorama_small.jpg',
+  },
+  {
+    src: '/images/projects/avalanche_crop.jpg',
   },
 ];
 
