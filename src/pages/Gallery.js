bc6c51356930421878fed9b493b0721b3fd071d7
Cyril Gadal <cyril.gadal@gmail.com>
2022-08-27 21:25:07 +0200

    Operating gallery

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -8,6 +8,10 @@ import lgThumbnail from 'lightgallery/plugins/thumbnail';
 
 import Main from '../layouts/Main';
 
+import data from '../data/gallery';
+
+const { PUBLIC_URL } = process.env; // set automatically from package.json:homepage
+
 const Gallery = () => (
   <Main
     title="Gallery"
@@ -20,56 +24,40 @@ const Gallery = () => (
         </div>
       </header>
       <LightGallery plugins={[lgZoom, lgVideo, lgThumbnail]} mode="lg-fade">
-        <a
-          data-lg-size="1406-1390"
-          className="gallery-item"
-          data-src="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1406&q=80"
-          data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@entrycube' >Diego Guzmán </a></h4> <p> Location - <a href='https://unsplash.com/s/photos/fushimi-inari-taisha-shrine-senbontorii%2C-68%E7%95%AA%E5%9C%B0-fukakusa-yabunouchicho%2C-fushimi-ward%2C-kyoto%2C-japan'>Fushimi Ward, Kyoto, Japan</a></p>"
-        >
-          <img
-            alt=""
-            className="img-responsive"
-            src="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80"
-          />
-        </a>
-        <a
-          data-lg-size="1400-1400"
-          data-pinterest-text="Shinimamiya, Osaka, Japan"
-          data-tweet-text="Shinimamiya, Osaka, Japan"
-          className="gallery-item"
-          data-src="https://images.unsplash.com/photo-1544550285-f813152fb2fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"
-          data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@asoshiation' >Shah </a></h4><p> Location - <a href='https://unsplash.com/s/photos/shinimamiya%2C-osaka%2C-japan'>Shinimamiya, Osaka, Japan</a></p>"
-        >
-          <img
-            alt=""
-            className="img-responsive"
-            src="https://images.unsplash.com/photo-1544550285-f813152fb2fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80"
-          />
-        </a>
-        <a
-          data-lg-size="1400-1400"
-          className="gallery-item"
-          data-src="https://images.unsplash.com/photo-1584592740039-cddf0671f3d4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"
-          data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@katherine_xx11' >Katherine Gu </a></h4><p> For all those years we were alone and helpless.</p>"
-        >
-          <img
-            alt=""
-            className="img-responsive"
-            src="https://images.unsplash.com/photo-1584592740039-cddf0671f3d4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80"
-          />
-        </a>
-        <a
-          className="gallery-item"
-          data-src="https://www.youtube.com/watch?v=9ZfN87gSjvI"
-          key="4"
-        >
-          <img
-            style={{ maxWidth: '400px' }}
-            className="img-responsive"
-            alt=""
-            src="https://img.youtube.com/vi/9ZfN87gSjvI/maxresdefault.jpg"
-          />
-        </a>
+        {(data).map((job) => {
+          const testVar = (job.type === 'video/mp4');
+
+          if (testVar) {
+            return (
+              <a
+                key={job._id}
+                className="gallery-item"
+                data-video={`{"source": [{"src": "${PUBLIC_URL}${job.src}", "type":"${job.type}"}], "attributes": {"preload": false, "controls": true}}`}
+                data-sub-html={job.description}
+              >
+                <img
+                  style={{ maxWidth: '400px' }}
+                  className="img-responsive"
+                  alt=""
+                  src={`${PUBLIC_URL}${job.thumb}`}
+                />
+              </a>);
+          }
+          return (
+            <a
+              key={job._id}
+              className="gallery-item"
+              href={`${PUBLIC_URL}${job.src}`}
+              data-sub-html={job.description}
+            >
+              <img
+                style={{ maxWidth: '400px' }}
+                className="img-responsive"
+                alt=""
+                src={`${PUBLIC_URL}${job.thumb}`}
+              />
+            </a>);
+        })}
       </LightGallery>
     </article>
   </Main>
