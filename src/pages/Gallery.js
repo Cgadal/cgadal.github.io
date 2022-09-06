ceb5836948f5f403f79f56e82d4d67d847138071
Cyril Gadal <cyril.gadal@gmail.com>
2022-09-06 12:54:49 +0200

    Fixing gallery responsivisity

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -36,7 +36,7 @@ const Gallery = () => (
                 data-sub-html={job.description}
               >
                 <img
-                  style={{ maxWidth: '100%', width: '250px' }}
+                  style={{ maxWidth: '100%', width: '400px' }}
                   className="img-responsive"
                   alt=""
                   src={`${PUBLIC_URL}${job.thumb}`}
@@ -51,7 +51,7 @@ const Gallery = () => (
               data-sub-html={job.description}
             >
               <img
-                style={{ maxWidth: '400px' }}
+                style={{ maxWidth: '100%', width: '400px'  }}
                 className="img-responsive"
                 alt=""
                 src={`${PUBLIC_URL}${job.thumb}`}
