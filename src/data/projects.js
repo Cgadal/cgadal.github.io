d0413dfb881eca878c8b6e360889cdd155a7d344
Cyril Gadal <cyril.gadal@gmail.com>
2023-02-21 19:23:02 +0100

    Correcting typos

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,20 +1,20 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'From a hydrodynamic instability to dune patterns building sand seas.',
+    title: 'From a hydrodynamic instability to dune patterns shaping sand seas.',
     image: '/images/projects/DunePanorama_small.jpg',
     date: '',
     desc: 'Dunes gathering at the bottom of a small mountain in the Taklamacan desert, in China [@ClémentNarteau]',
     content: 'Sand seas are easily recognisable across our solar system thanks'
-    + ' to the patterns resulting form the self-organization of the dunes that compose them.'
-    + ' However, a quantitative descriptions of how these patterns emerge and evolve is only available'
-    + ' in the simplified situations (constant unidirectional wind, homogenous spatial conditions, etc ..).'
-    + ' We therefore aim to improve these theoretical descriptions by integrating the influence of various external parameters,'
-    + ' such as the wind seasonal variations or the conditions at the edge of the dune field'
-    + ' By doing so, reliable interpretation of field data in many dfferent situations can be achieved,'
-    + ' leading for exemple to a better prediction of the evolution of dunes and sand seas,'
-    + ' but also to the inference of environmental parameters (wind regime, sediment properties)'
-    + ' from dune observations in satellite images in places where direct are not feasible, such as Mars or Vénus.',
+      + ' to the patterns resulting form the self-organization of the dunes that compose them.'
+      + ' However, a quantitative descriptions of how these patterns emerge and evolve is only available'
+      + ' in the simplified situations (constant unidirectional wind, homogenous spatial conditions, etc ..).'
+      + ' We therefore aim to improve these theoretical descriptions by integrating the influence of various external parameters,'
+      + ' such as the wind seasonal variations or the conditions at the edge of the dune field'
+      + ' By doing so, reliable interpretation of field data in many dfferent situations can be achieved,'
+      + ' leading for exemple to a better prediction of the evolution of dunes and sand seas,'
+      + ' but also to the inference of environmental parameters (wind regime, sediment properties)'
+      + ' from dune observations in satellite images in places where direct are not feasible, such as Mars or Vénus.',
     points: [
       'Theoretical description of dune morphodynamics (instabilities, non-linearities)',
       'Numerical work and experiments',
@@ -22,22 +22,22 @@ const data = [
     ],
   },
   {
-    title: 'Particle-ladden gravity currents.',
+    title: 'Particle-laden gravity currents.',
     image: '/images/projects/avalanche_crop.jpg',
     date: '',
     desc: 'Powder snow avalanche at the Zinal ski station, in Switzerland.',
     content: 'Particle-ladden gravity currents, or turbidity currents, are currents'
-    + ' driven by a density difference induced by the presence of particles in'
-    + ' suspension (e.g. sand, snow, dust). They exist in various environments,'
-    + ' such as in oceans, where sand mixes with water to generate currents able'
-    + ' to destroy submarine infrastuctures, or near volcanos, where the hot ashes realeased'
-    + ' during eruptions transform into deadly pyroclastic currents.'
-    + ' Their dynamics involves interactions across a whole range of spatial and temporal scales,'
-    + ' from between the particles and the flow turbulent structures to between the current'
-    + ' itself and the underlying landscape topography.'
-    + ' In the lab, we study simplified situations, trying to isolate the impact of each process '
-    + ' on the current dynamics. This helps creating models (i.e depth-averaged), that can be further used'
-    + ' to assess destructive potential of real/natural situations.',
+      + ' driven by a density difference induced by the presence of particles in'
+      + ' suspension (e.g. sand, snow, dust). They exist in various environments,'
+      + ' such as in oceans, where sand mixes with water to generate currents able'
+      + ' to destroy submarine infrastuctures, or near volcanos, where the hot ashes realeased'
+      + ' during eruptions transform into deadly pyroclastic currents.'
+      + ' Their dynamics involves interactions across a whole range of spatial and temporal scales,'
+      + ' from between the particles and the flow turbulent structures to between the current'
+      + ' itself and the underlying landscape topography.'
+      + ' In the lab, we study simplified situations, trying to isolate the impact of each process '
+      + ' on the current dynamics. This helps creating models (i.e depth-averaged), that can be further used'
+      + ' to assess destructive potential of real/natural situations.',
     points: [
       'Shallow water modelling',
       'Experimental work',
