16209cdb47ab226ade8bbc907ffb3aeb61fba4f4
Cyril Gadal <cyril.gadal@gmail.com>
2023-03-23 09:39:46 +0100

    Adding datasets

--- src/data/publications/datasets.js
+++ src/data/publications/datasets.js
@@ -1,6 +1,13 @@
 const datasets = [
   {
-    title: "Data used in 'Local Wind Regime Induced by Giant Linear Dunes: Comparison of ERA5-Land Reanalysis with Surface Measurements'  .",
+    title: "Data used in 'Slumping regime in lock-release turbidity currents'.",
+    date: 2022,
+    author_list: 'Gadal, C., Mercier, M. and Lacaze, L.',
+    repository: 'Zenodo',
+    doi: 'https://doi.org/10.5281/zenodo.7487190',
+  },
+  {
+    title: "Data used in 'Local Wind Regime Induced by Giant Linear Dunes: Comparison of ERA5-Land Reanalysis with Surface Measurements'.",
     date: 2022,
     author_list: 'Gadal, C., Delorme, P., Narteau, C., Wiggs, F.S.W., Baddock, M., Nield, J.M. and Claudin, P.',
     repository: 'Zenodo',
